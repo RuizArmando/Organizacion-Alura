ed67c8ebbe41b3b74ee0bc8e10a5f5dd744f10fc
Armando Ruiz <ruiz12armando@gmail.com>
2023-09-21 16:18:38 -0600

    Crecion del evento para mostrar formulario

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
+import { useState } from 'react';
 import './App.css';
 import Header from './componentes/Header/Header.js';
 import Formulario from './componentes/Formulario/Formulario.js';
 import MiOrg from "./componentes/MiOrg/MiOrg.js"
 
 function App() {
+  const [mostrarFormulario, actualizarmostrar] = useState(true)
+
+  const cambiarMostrar = () => {
+    actualizarmostrar(!mostrarFormulario)
+  }
+
   return (
     <div className="App">
       <Header/>
-      <Formulario/>
-      <MiOrg />
+      {mostrarFormulario && <Formulario />}
+      <MiOrg cambiarMostrar={cambiarMostrar}/>
     </div>
   );
 }
--- src/componentes/MiOrg/MiOrg.js
+++ src/componentes/MiOrg/MiOrg.js
@@ -1,9 +1,10 @@
+import { useState } from "react"
 import "./MiOrg.css"
 
-const MiOrg = () =>{
+const MiOrg = (props) =>{
     return <section className="orgSection">
         <h3 className="title">Mi Organizacion</h3>
-        <img src='/img/add.png' alt='add'/>
+        <img src='/img/add.png' alt='add' onClick={props.cambiarMostrar}/>
     </section>
 }
 
