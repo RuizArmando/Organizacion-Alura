48d0b2488e4971d8ce338924b0b1ff4468a3a4aa
Armando Ruiz <ruiz12armando@gmail.com>
2023-09-22 15:24:47 -0600

    Se modifica el formulario

--- src/componentes/Formulario/Formulario.js
+++ src/componentes/Formulario/Formulario.js
@@ -1,20 +1,36 @@
+import { useState } from "react"
 import "./Formulario.css"
 import CampoTexto from "../CampoTexto/CampoTexto.js"
 import ListaObciones from "../ListaObciones/ListaObciones.js"
 import Boton from "../Boton/Boton.js"
 
-const Formulario = () =>{
+const Formulario = (props) =>{
+    const [nombre, actualizarNombre] = useState("")
+    const [puesto, actualizarPuesto] = useState("")
+    const [foto, actualizarFoto] = useState("")
+    const [equipo, actualizarEquipo] = useState("")
+
+    //const { registrarColaborador } = props
+
     const manejoEnvio = (e) =>{
         e.preventDefault();
-        console.log("Manejar el envio", e);
+        console.log("Manejar el envio")
+        let datosAEnviar = {
+            nombre,
+            puesto,
+            foto,
+            equipo
+        }
+        //registrarColaborador(datosAEnviar)
+        console.log(datosAEnviar)
     }
 return <section className="formulario">
             <form onSubmit={manejoEnvio}>
                 <h2>Rellena el formulario para el colaborador.</h2>
-                <CampoTexto titulo="Nombre" placeholder="Ingresar nombre" required/>
-                <CampoTexto titulo="Puesto" placeholder="Ingresar puesto" required/>
-                <CampoTexto titulo="Foto" placeholder="Ingresar enlace de foto" />
-                <ListaObciones />
+                <CampoTexto titulo="Nombre" placeholder="Ingresar nombre" required valor={nombre} actualizarValor={actualizarNombre}/>
+                <CampoTexto titulo="Puesto" placeholder="Ingresar puesto" required valor={puesto} actualizarValor={actualizarPuesto}/>
+                <CampoTexto titulo="Foto" placeholder="Ingresar enlace de foto" valor={foto} actualizarValor={actualizarFoto}/>
+                <ListaObciones valor={equipo} actualizarEquipo={actualizarEquipo} equipos={props.equipos} />
                 <Boton texto="Crear colaborador" />
             </form>
         </section>
--- src/componentes/ListaObciones/ListaObciones.js
+++ src/componentes/ListaObciones/ListaObciones.js
@@ -11,10 +11,16 @@ const ListaObciones = (props) => {
         "Innovación y Gestión"
     ]
 
+    const manejarCambio = (e) =>{
+        console.log("Cambio", e.target.value)
+        props.actualizarEquipo(e.target.value)
+    }
+
     return <div className="lista-opciones">
         <label>Equipos</label>
-        <select>
-            {equipos.map((equipo, index) => <option key={index}>{equipo} </option>)}
+        <select value={props.valor} onChange={manejarCambio}>
+            <option value="" disabled defaultValue="" hidden>Seleccionar equipo</option>
+            {equipos.map((equipo, index) => <option key={index} value={equipo}>{equipo} </option>)}
         </select>
     </div>
 }
